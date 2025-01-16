40bf7a3979f8a2aad2bd91df68099332f9d0468c
sandeshsamgir05 <sandesh.samgir05@gmail.com>
2025-01-16 22:37:33 +0530

    Parse JSON

--- src/Basics.java
+++ src/Basics.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -11,15 +13,21 @@ public class Basics {
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
 
         // Given-When-Then structure for API request
-        given()
+        String response = given()
             .queryParam("key", "qaclick123") // Note: 'key' is case-sensitive
             .header("Content-Type", "application/json")
             .body(payload.AddPlace())
         .when().post("maps/api/place/add/json").then().log().all()
-            .assertThat().statusCode(200).body("scope", equalTo("APP")).header("server","Apache/2.4.52 (Ubuntu)");
+            .assertThat().statusCode(200).body("scope", equalTo("APP")).header("server","Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
+        System.out.println(response);
+        
     //add place > UpdatePlace with new address> Get Place to validate if the new address is updated.
         
+        JsonPath js = new JsonPath(response); //for parsing JSON
+        String placeID = js.getString("place_id");
+        
+        System.out.println(placeID);
         
         
     
