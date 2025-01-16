4fecb57bb6ab4040af602e0524c775b739e86a58
sandeshsamgir05 <sandesh.samgir05@gmail.com>
2025-01-16 22:29:33 +0530

    add place api

--- src/Basics.java
+++ src/Basics.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import Files.payload;
 
 public class Basics {
 
@@ -11,28 +14,14 @@ public class Basics {
         given()
             .queryParam("key", "qaclick123") // Note: 'key' is case-sensitive
             .header("Content-Type", "application/json")
-            .body("{\r\n" +
-                  "  \"location\": {\r\n" +
-                  "    \"lat\": -38.383494,\r\n" +
-                  "    \"lng\": 33.427362\r\n" +
-                  "  },\r\n" +
-                  "  \"accuracy\": 50,\r\n" +
-                  "  \"name\": \"sandesh place\",\r\n" +
-                  "  \"phone_number\": \"(+91) 983 893 3937\",\r\n" +
-                  "  \"address\": \"29, side layout, cohen 09\",\r\n" +
-                  "  \"types\": [\r\n" +
-                  "    \"shoe park\",\r\n" +
-                  "    \"shop\"\r\n" +
-                  "  ],\r\n" +
-                  "  \"website\": \"http://google.com\",\r\n" +
-                  "  \"language\": \"French-IN\"\r\n" +
-                  "}")
-        .when()
+            .body(payload.AddPlace())
+        .when().post("maps/api/place/add/json").then().log().all()
+            .assertThat().statusCode(200).body("scope", equalTo("APP")).header("server","Apache/2.4.52 (Ubuntu)");
+
+    //add place > UpdatePlace with new address> Get Place to validate if the new address is updated.
         
-            .post("maps/api/place/add/json")
-        .then()
-            .log().all()
-            .assertThat()
-            .statusCode(200);
+        
+        
+    
     }
 }
